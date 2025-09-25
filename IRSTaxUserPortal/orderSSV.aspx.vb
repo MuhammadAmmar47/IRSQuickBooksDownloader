ba6428ef91d7f3dc06e543119e247b2da1de1a13
Sami Javed <javedsami061@gmail.com>
2025-09-25 23:05:06 +0500

    Changes

--- IRSTaxUserPortal/orderSSV.aspx.vb
+++ IRSTaxUserPortal/orderSSV.aspx.vb
@@ -7,45 +7,55 @@ Public Class orderSSV
 
     End Sub
     Protected Sub btnSubmit_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnSubmit.Click
+        'If Not ValidateForm() Then Return
         Dim savedFilePath As String = Nothing
         If ssa89File.HasFile Then
             savedFilePath = order_4506.UploadFile(ssa89File.PostedFile, "~/Uploads/")
         End If
 
-        ' Collect form values
-        Dim requestName As String = txtTaxPayerName.Text.Trim()
-        Dim ssnNumber As String = txtSocialSecurityNumber.Text.Trim()
-        Dim loanNumber As String = txtLoanNumber.Text.Trim()
-        Dim dob As String = If(String.IsNullOrWhiteSpace(txtDob.Text), Nothing, txtDob.Text.Trim())
+        Dim resultOrderIDs As New Generic.List(Of Integer)
         Dim gender As String = ""
         If rbMale.Checked Then
             gender = rbMale.Value
         ElseIf rbFemale.Checked Then
             gender = rbFemale.Value
         End If
-        ' Tax year checkboxes
-        Dim taxYear2024 As String = Nothing
-        Dim taxYear2023 As String = Nothing
-        Dim taxYear2022 As String = Nothing
-        Dim taxYear2021 As String = Nothing
-        Dim taxYear2020 As String = Nothing
+        Dim o As New Orders.Order
+        With o
+            .fldCompanyID = StoreInstance.GetCustomerId()
+            .fldcustomeriD = StoreInstance.GetCustomerId()
+            .fldemail = "OFF"
+            .fldfax = "OFF"
+            .fldfaxno = ""
+            .fldListid = 0
+            .fldlisttype = 1
+            .fldLoanNumber = Me.txtLoanNumber.Text.Trim
+            .fldOrderdate = Now.ToShortDateString
+            .fldrequestname = txtTaxPayerName.Text.Trim()
+            .fldssnno = txtSocialSecurityNumber.Text.Trim()
+            .fldstatus = "p"
+            .fldPdf = ssa89File.PostedFile.FileName
+            .fldTaxyear2020 = Nothing
+            .fldTaxyear2021 = Nothing
+            .fldTaxyear2022 = Nothing
+            .fldTaxyear2023 = Nothing
+            .fldTaxyear2024 = Nothing
+            .fldListid = 0
+            .fldlisttype = 0
+            .FormType = 0
+            .fldordernumber = 0
+            .fldDOB = If(String.IsNullOrWhiteSpace(txtDob.Text), Nothing, txtDob.Text.Trim())
+            .fldSex = gender
 
-        Dim newListId As Integer = 1
-        Dim listType As Integer = 1
-        Dim typeOfForm As String = "SSA-89"
-        Dim email As String = ""
-        Dim fax As String = ""
-        Dim faxNo As String = ""
-        Dim orderDate As DateTime = DateTime.Now
-        Dim customerId As Integer = StoreInstance.GetCustomerId()
-        Dim companyId As Integer = StoreInstance.GetCustomerId()
-        ' Insert into DB
-        order_4506.InsertOrder(newListId, listType, customerId, requestName, ssnNumber,
-                taxYear2024, taxYear2023, taxYear2022, taxYear2021, taxYear2020,
-                typeOfForm, email, fax, faxNo, orderDate, companyId, loanNumber, ssa89File.PostedFile.FileName, dob, gender
-                )
+            OrderServices.CreateNewOrder(o)
+                If o.fldordernumber < 1 Then
+                    'msg.ShowError("Failed to save order. " & DataHelper.LastErrorMessage)
+                End If
+            resultOrderIDs.Add(o.fldordernumber)
+        End With
 
         Response.Redirect("~/Confirmation.aspx")
+        'msg.ShowInformation(resultOrderIDs.Count & " Orders created. Order numbers are " & Utilities.Translators.GenericArrayToString(resultOrderIDs))
     End Sub
 
 End Class
